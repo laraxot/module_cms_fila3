0176090ba4db7ed0c74a95b4849e699a7d91cf1d
Malebestia <vair81@gmail.com>
2023-11-17 19:02:32 +0100

    up

--- Routes/web.php
+++ Routes/web.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\Broadcast;
-
-@require __DIR__.'/web_01.php';
-
-Broadcast::routes();
+Route::get('/', function () {
+    return view('welcome');
+});
