82e5c66e515128bf7ef553ee9979c608f5f9dc07
marco76tv <marco.sottana@gmail.com>
2023-11-20 22:30:32 +0100

    up

--- Routes/web.php
+++ Routes/web.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Route;
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/{lang?}/{container0?}', '\Modules\Cms\Filament\Front\Pages\Welcome')->name('test');
