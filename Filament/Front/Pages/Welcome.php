7591b7eac86e926886d6683c599c6d762e2da69c
marco76tv <marco.sottana@gmail.com>
2023-11-20 23:21:18 +0100

    ..

--- Filament/Front/Pages/Welcome.php
+++ Filament/Front/Pages/Welcome.php
@@ -1,16 +1,96 @@
 <?php
+/**
+ * @see https://laraveldaily.com/post/filament-custom-edit-profile-page-multiple-forms-full-design
+ */
 
 declare(strict_types=1);
 
 namespace Modules\Cms\Filament\Front\Pages;
 
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Pages\Page;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+use Modules\Blog\Models\Post;
+use Modules\Quaeris\Models\QuestionChart;
 
-class Welcome extends Page
+class Welcome extends Page implements HasTable
 {
+    use InteractsWithTable;
+    // use InteractsWithForms;
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'cms::filament.front.pages.welcome';
 
     protected static string $layout = 'pub_theme::layouts.app';
+
+    protected function getTables(): array
+    {
+        return [
+            // 'articlesTable',
+        ];
+    }
+
+    public function articlesTable(Infolist $infolist): Infolist
+    {
+        /*
+        return $table
+            ->columns([
+                TextColumn::make('title'),
+
+            ]);
+            */
+        return $infolist
+            ->schema([
+                TextEntry::make('title'),
+                TextEntry::make('slug'),
+                // ...
+            ])
+            ->record(Post::first())
+            ->columns(2);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->query(QuestionChart::query())
+
+            ->contentGrid([
+                'md' => 2,
+                'xl' => 3,
+            ])
+
+            ->columns([
+                Split::make([
+                    TextColumn::make('id')
+                        ->label('#'),
+                    TextColumn::make('question'),
+                    /*TextColumn::make('lessons_count')
+                    ->label('Lessons')
+                    ->counts('lessons'),
+                    */
+                ]),
+            ])
+            ->actions([
+                /*Action::make('Lessons')
+                    ->icon('heroicon-m-academic-cap')
+                    ->url(fn (Course $record): string => LessonResource::getUrl('index', [
+                        'tableFilters[course][value]' => $record,
+                    ])),*/
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
 }
--- Routes/web.php
+++ Routes/web.php
@@ -4,10 +4,4 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
-
 Route::get('/{lang?}/{container0?}', '\Modules\Cms\Filament\Front\Pages\Welcome')->name('test');
