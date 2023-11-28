06691f51d71a54e8e7bdfeca71d64a0eef603807
marco76tv <marco.sottana@gmail.com>
2023-11-28 17:19:57 +0100

    .

--- Routes/web.php
+++ Routes/web.php
@@ -6,4 +6,4 @@ use Modules\Cms\Filament\Front\Pages\Welcome;
 
 // Route::get('/{lang?}/{container0?}', '\\' . Welcome::class)->name('test');
 
-Route::resource('{container0}/{item0?}/{container1?}/{item1?}/{container2?}/{item2?}', PageController::class);
+Route::get('{container0}/{item0?}/{container1?}/{item1?}/{container2?}/{item2?}', PageController::class);
