5d38b7f668bec37a35e6d626d66c701313234689
marco76tv <marco76tv@users.noreply.github.com>
2024-01-08 17:50:38 +0000

    Check & fix styling

--- Filament/Front/Pages/Home.php
+++ Filament/Front/Pages/Home.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class Home extends Page // implements HasTable
-{
-    // use InteractsWithTable;
+{// use InteractsWithTable;
     // use InteractsWithForms;
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
