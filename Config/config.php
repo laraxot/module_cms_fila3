ba7b96ca4f9cc0f4c094c143e9e25239a9bd8bd9
marco76tv <marco.sottana@gmail.com>
2024-01-09 16:20:36 +0100

    .

--- Config/config.php
+++ Config/config.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 return [
     'name' => 'Cms',
-<<<<<<< HEAD
-    // 'icon'
-=======
     'icon' => 'heroicon-o-cog', // icon on dashboard
     'navigation_sort' => 1,
->>>>>>> 56947e3 (up)
 ];
