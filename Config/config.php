18bfdb7099c5529efe871c47efb9878ee98f41ee
marco76tv <marco.sottana@gmail.com>
2024-01-09 16:19:42 +0100

    .

--- Config/config.php
+++ Config/config.php
@@ -4,5 +4,10 @@ declare(strict_types=1);
 
 return [
     'name' => 'Cms',
+<<<<<<< HEAD
     // 'icon'
+=======
+    'icon' => 'heroicon-o-cog', // icon on dashboard
+    'navigation_sort' => 1,
+>>>>>>> 56947e3 (up)
 ];
--- Routes/web.php
+++ Routes/web.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Modules\Cms\Filament\Front\Pages\Home;
 use Modules\Cms\Filament\Front\Pages\Welcome;
-use Modules\Cms\Http\Controllers\PageController;
+//use Modules\Cms\Http\Controllers\PageController;
 
 Route::get('/{lang?}/{container0?}/{item0?}/{container1?}/{item1?}/{container2?}/{item2?}', '\\'.Welcome::class)->name('test');
 Route::get('/', '\\'.Home::class)->name('home');
