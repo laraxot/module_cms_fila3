cbe6725b4b6b68128b1c5d4cb2426476a6f43762
marco76tv <marco.sottana@gmail.com>
2024-02-20 14:22:49 +0100

    up

--- Providers/CmsServiceProvider.php
+++ Providers/CmsServiceProvider.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Providers;
 
-use Exception;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\View;
-use Modules\Tenant\Services\TenantService;
-use Modules\UI\Services\ThemeService;
 use Modules\Xot\Datas\XotData;
 use Modules\Xot\Providers\XotBaseServiceProvider;
-use Modules\Xot\Services\BladeService;
 use Modules\Xot\Services\FileService;
+use Modules\Xot\Services\LivewireService;
 
 /**
  * Undocumented class.
@@ -30,25 +26,17 @@ class CmsServiceProvider extends XotBaseServiceProvider
 
     public function bootCallback(): void
     {
-        // BladeService::registerComponents($this->module_dir.'/../View/Components', 'Modules\\Cms');
-
         $this->xot = XotData::make();
-
-        if ($this->xot->register_adm_theme) {
-            $this->registerNamespaces('adm_theme');
-            $this->registerThemeConfig('adm_theme');
-        }
 
         if ($this->xot->register_pub_theme) {
             $this->registerNamespaces('pub_theme');
-            $this->bootThemeProvider('pub_theme');
+
             $this->registerThemeConfig('pub_theme');
+            $this->registerThemeLivewireComponents();
         }
 
-        // $this->registerViewComposers();
-
         $timezone = config('app.timezone') ?? 'Europe/Berlin';
-        if (! is_string($timezone)) {
+        if (! \is_string($timezone)) {
             throw new \Exception('['.__LINE__.']['.__FILE__.']');
         }
 
@@ -59,6 +47,20 @@ class CmsServiceProvider extends XotBaseServiceProvider
     {
         $configFileName = 'xra';
         $this->mergeConfigFrom(__DIR__.sprintf('/../Config/%s.php', $configFileName), $configFileName);
+    }
+
+    /**
+     * Undocumented function.
+     */
+    public function registerThemeLivewireComponents(): void
+    {
+        // $prefix=$this->module_name.'::';
+        $prefix = '';
+        LivewireService::registerComponents(
+            base_path('Themes/'.$this->xot->pub_theme.'/Http/Livewire'),
+            'Themes\\'.$this->xot->pub_theme,
+            $prefix,
+        );
     }
 
     /**
@@ -79,48 +81,10 @@ class CmsServiceProvider extends XotBaseServiceProvider
         $this->loadTranslationsFrom($lang_dir, $theme_type);
     }
 
-    /**
-     * Undocumented function.
-     *
-     * @return void
-     */
-    public function bootThemeProvider(string $theme_type)
-    {
-        if ($this->app->runningInConsole()) {
-            return;
-        }
-
-        $xot = $this->xot;
-
-        $theme = $xot->{$theme_type};
-        if (! File::exists(base_path('Themes/'.$theme))) {
-            // $xot->{$theme_type} = ThemeService::firstThemeName($theme_type);
-            // TenantService::saveConfig('xra', $xot->toArray());
-            throw new \Exception('['.base_path('Themes/'.$theme).' not exists]['.__LINE__.']['.class_basename(self::class).']');
-        }
-
-        /* //in attesa di vedere se serve di nuovo.
-        $provider = 'Themes\\'.$theme.'\Providers\\'.$theme.'ServiceProvider';
-        if (! class_exists($provider)) {
-            throw new Exception('class not exists ['.$provider.']['.__LINE__.']['.basename(__FILE__).']');
-        }
-
-        $provider = new $provider();
-
-        if (method_exists($provider, 'bootCallback')) {
-            $provider->bootCallback();
-        }
-        */
-    }
-
     public function registerThemeConfig(string $theme_type): void
     {
         $xot = $this->xot;
 
-        // if (! isset($xot[$theme_type])) {
-        //    $xot[$theme_type] = ThemeService::firstThemeName($theme_type);
-        // TenantService::saveConfig('xra',$xot);
-        // }
         $theme = $xot->{$theme_type};
 
         $config_path = base_path('Themes/'.$theme.'/Config');
@@ -140,37 +104,4 @@ class CmsServiceProvider extends XotBaseServiceProvider
             Config::set($theme_type.'::'.$name, $data);
         }
     }
-
-    /*
-     * Undocumented function.
-    *  Method Modules\Cms\Providers\CmsServiceProvider::registerViewComposers() is unused.
-    private function registerViewComposers(): void
-    {
-
-        //if ($this->app->runningInConsole()) {
-        //    return;
-        //}
-
-        $xot = $this->xot;
-
-        $theme = \inAdmin() ? $xot->adm_theme : $xot->pub_theme;
-        if (null == $theme) {
-            throw new \Exception('iuston gavemo un problema ['.__LINE__.']['.class_basename(self::class).']');
-        }
-
-        $custom_composer = '\Themes\\'.$theme.'\View\Composers\ThemeComposer';
-        if (class_exists($custom_composer)) {
-            View::composer('*', $custom_composer);
-
-            return;
-        }
-
-        if ($this->app->runningInConsole()) {
-            return;
-        }
-
-        throw new \Exception('add ['.$custom_composer.']');
-        // View::composer('*', ThemeComposer::class);
-    }
-    */
 }
