73ffb6a4ea50c668f786dc85925d161f91fbb2d6
Malebestia <vair81@gmail.com>
2024-08-13 12:55:52 +0200

    cluster Appearance wip

---
+++ Filament/Clusters/Appearance.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Cms\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class Appearance extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+}
