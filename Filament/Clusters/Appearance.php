f09da77bb4fd93ada2c2c7b1ffbbdd788f7a068e
Malebestia <vair81@gmail.com>
2024-08-13 18:53:52 +0200

    wip appearance theme

--- Filament/Clusters/Appearance.php
+++ Filament/Clusters/Appearance.php
@@ -9,4 +9,6 @@ use Filament\Clusters\Cluster;
 class Appearance extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    protected static ?string $navigationGroup = 'Settings';
 }
