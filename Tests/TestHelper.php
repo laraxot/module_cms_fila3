f976cb078ba967083563b39e7effe935e282f8e2
marco <marco.sottana@gmail.com>
2024-08-21 15:47:19 +0200

    🐛 (FixJigSawByModuleAction.php): Fix comparison operator in if conditions for better readability 📝 (ThemeData.php): Add default values for class properties in ThemeData to improve code clarity 🔧 (Headernav.php): Resolve conflicts and clean up unnecessary code in Headernav.php 🔧 (Home.php): Update comparison operators for better code consistency in Home.php 🔧 (Welcome.php): Update comparison operators for better code consistency in Welcome.php 🔧 (Themes.php): Fix indentation and formatting issues in Themes.php 🔧 (CreatePage.php): Remove unnecessary whitespace in CreatePage.php 🔧 (EditPage.php): Remove unnecessary whitespace in EditPage.php 🔧 (ListPages.php): Fix order of traits and clean up code in ListPages.php 🔧 (XotPanelController.php): Update parameter annotations for XotPanelController.php 🔧 (BaseModel.php): Add whitespace for better code readability in BaseModel.php 🔧 (Conf.php): Update property annotation formatting in Conf.php
    📝 (Home.php): Update property annotations for better readability and consistency
    💡 (Home.php): Add property annotations for creator and updater relationships for better documentation and IDE support
    
    📝 (Menu.php): Update property names and method signatures for better readability and consistency. Add support for creator and updater profiles.
    
    ♻️ Remove unnecessary parentheses when instantiating a class instance
    
    📝 (Models/Module.php): Update property formatting and add creator and updater properties
    📝 (Models/Page.php): Update property formatting and add creator and updater properties
    📝 (Providers/CmsServiceProvider.php): Update comparison operator in if statement
    📝 (Providers/EventServiceProvider.php): Remove empty configureEmailVerification method
    📝 (Tests/TestHelper.php): Update comparison operator in if statement
    📝 (View/Composers/ThemeComposer.php): Update comparison operators in if statements and improve code readability

--- Tests/TestHelper.php
+++ Tests/TestHelper.php
@@ -53,7 +53,7 @@ abstract class TestHelper extends BaseTestCase
     public function getUserNavigationItemUrlRoles($user)
     {
         return $role_names = $user->getRoleNames()->map(function ($item) {
-            if ('super-admin' != $item) {
+            if ($item != 'super-admin') {
                 return '/'.substr($item, 0, -7).'/admin';
             }
         })->filter(function ($value) {
