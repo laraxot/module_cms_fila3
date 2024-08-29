cf69ef98f3a7278ce77fc65404fc2094ae3c560a
marco <marco.sottana@gmail.com>
2024-08-29 10:04:04 +0200

    🔧 (Headernav.php): Remove unnecessary code and comments for better code readability and maintainability.
    📝 (Menu.php): Improve property formatting for better readability and consistency
    ♻️ (Menu.php): Refactor method signatures for better alignment and readability
    
    🔧 (Menu.php): Fix missing parentheses in object instantiation to prevent potential errors
    
    📝 (Module.php): Improve code readability by aligning property and method annotations in Module.php
    📝 (Page.php): Improve code readability by aligning property and method annotations in Page.php

--- Models/Page.php
+++ Models/Page.php
@@ -11,39 +11,39 @@ use Spatie\Translatable\HasTranslations;
 /**
  * Modules\Cms\Models\Page.
  *
- * @property string $id
+ * @property string                          $id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string $slug
- * @property string $title
- * @property string $content
- * @property string|null $updated_by
- * @property string|null $created_by
+ * @property string                          $slug
+ * @property string                          $title
+ * @property string                          $content
+ * @property string|null                     $updated_by
+ * @property string|null                     $created_by
  * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null $deleted_by
- * @property array|null $content_blocks
+ * @property string|null                     $deleted_by
+ * @property array|null                      $content_blocks
  *
  * @method static \Modules\Blog\Database\Factories\PageFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Page newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Page newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Page onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|Page query()
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereContent($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereContentBlocks($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page whereUpdatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Page withTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|Page withoutTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereContentBlocks($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   withTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page   withoutTrashed()
  *
  * @property array|null $sidebar_blocks
- * @property array $footer_blocks
+ * @property array      $footer_blocks
  *
  * @method static \Illuminate\Database\Eloquent\Builder|Page whereFooterBlocks($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Page whereSidebarBlocks($value)
@@ -55,13 +55,8 @@ use Spatie\Translatable\HasTranslations;
  * @method static \Illuminate\Database\Eloquent\Builder|Page whereJsonContainsLocale(string $column, string $locale, ?mixed $value)
  * @method static \Illuminate\Database\Eloquent\Builder|Page whereJsonContainsLocales(string $column, array $locales, ?mixed $value)
  *
-<<<<<<< HEAD
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
-=======
- * @property-read \Modules\Fixcity\Models\Profile|null $creator
- * @property-read \Modules\Fixcity\Models\Profile|null $updater
->>>>>>> c2c3521 (🐛 (FixJigSawByModuleAction.php): Fix comparison operator in if conditions for better readability)
  *
  * @mixin \Eloquent
  */
