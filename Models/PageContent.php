8cb4cc3b19f11063f8cd150b580c0ca27a119a92
marco <marco.sottana@gmail.com>
2024-10-14 10:01:26 +0200

    Check & fix styling

--- Models/PageContent.php
+++ Models/PageContent.php
@@ -11,19 +11,22 @@ use Spatie\Translatable\HasTranslations;
 /**
  * Modules\Cms\Models\PageContent.
  *
- * @property string $blocks
- * @property string $blocks
- * @property string|null $id
- * @property array|null $name
- * @property string|null $slug
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $created_by
- * @property string|null $updated_by
+ * @property string                               $blocks
+ * @property string                               $blocks
+ * @property string|null                          $id
+ * @property array|null                           $name
+ * @property string|null                          $slug
+ * @property \Illuminate\Support\Carbon|null      $created_at
+ * @property \Illuminate\Support\Carbon|null      $updated_at
+ * @property string|null                          $created_by
+ * @property string|null                          $updated_by
  * @property \Modules\Idoteca\Models\Profile|null $creator
- * @property mixed $translations
+ * @property mixed                                $translations
  * @property \Modules\Idoteca\Models\Profile|null $updater
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b0da697 (Check & fix styling)
  * @property string                               $blocks
  * @property string|null                          $id
  * @property array|null                           $name
@@ -60,6 +63,7 @@ use Spatie\Translatable\HasTranslations;
  * @property ProfileContract|null                 $creator
  * @property mixed                                $translations
  * @property ProfileContract|null                 $updater
+<<<<<<< HEAD
 <<<<<<< HEAD
  * @property string                               $blocks
  * @property string|null                          $id
@@ -126,23 +130,25 @@ use Spatie\Translatable\HasTranslations;
  * @property mixed $translations
  * @property ProfileContract|null $updater
 >>>>>>> 0235f62 (.)
+=======
+>>>>>>> b0da697 (Check & fix styling)
  *
  * @method static \Modules\Cms\Database\Factories\PageContentFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent query()
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereBlocks($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereJsonContainsLocale(string $column, string $locale, ?mixed $value, string $operand = '=')
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereJsonContainsLocales(string $column, array $locales, ?mixed $value, string $operand = '=')
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereLocale(string $column, string $locale)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereLocales(string $column, array $locales)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PageContent whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereBlocks($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereJsonContainsLocale(string $column, string $locale, ?mixed $value, string $operand = '=')
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereJsonContainsLocales(string $column, array $locales, ?mixed $value, string $operand = '=')
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereLocale(string $column, string $locale)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereLocales(string $column, array $locales)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PageContent  whereUpdatedBy($value)
  *
  * @mixin \Eloquent
  */
