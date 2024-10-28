59dc58c2cee6b60253e1638dd91226020e1b0a8c
marco <marco.sottana@gmail.com>
2024-10-28 12:23:22 +0100

    up

--- Models/Menu.php
+++ Models/Menu.php
@@ -162,7 +162,7 @@ class Menu extends BaseModel
     }
 
     /** @return array<string, string> */
-    protected function casts(): array
+    public function casts(): array
     {
         return [
             'id' => 'string',
