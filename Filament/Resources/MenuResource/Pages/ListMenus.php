43ba728e7988e35ed57ce58ebb7ef44a1dbe0e81
marco76tv <marco.sottana@gmail.com>
2024-11-14 17:53:37 +0100

    up

--- Filament/Resources/MenuResource/Pages/ListMenus.php
+++ Filament/Resources/MenuResource/Pages/ListMenus.php
@@ -7,8 +7,9 @@ namespace Modules\Cms\Filament\Resources\MenuResource\Pages;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Modules\Cms\Filament\Resources\MenuResource;
+use Modules\Xot\Filament\Pages\XotBaseListRecords;
 
-class ListMenus extends ListRecords
+class ListMenus extends XotBaseListRecords
 {
     protected static string $resource = MenuResource::class;
 
