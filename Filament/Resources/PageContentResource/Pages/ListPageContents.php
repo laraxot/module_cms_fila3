8e523fe7e8513aa46b12bea6e9e04c0119a1ca0d
marco76tv <marco76tv@users.noreply.github.com>
2024-11-14 20:34:38 +0000

    Check & fix styling

--- Filament/Resources/PageContentResource/Pages/ListPageContents.php
+++ Filament/Resources/PageContentResource/Pages/ListPageContents.php
@@ -5,27 +5,25 @@ declare(strict_types=1);
 namespace Modules\Cms\Filament\Resources\PageContentResource\Pages;
 
 use Filament\Actions;
-use Filament\Tables\Table;
 use Filament\Actions\CreateAction;
-use Modules\UI\Enums\TableLayoutEnum;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ViewAction;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Enums\FiltersLayout;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Columns\Layout\Stack;
-use Filament\Tables\Enums\ActionsPosition;
-use Modules\Xot\Filament\Traits\TransTrait;
 use Filament\Tables\Actions\DeleteBulkAction;
-use Modules\Xot\Filament\Pages\XotBaseListRecords;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\ActionsPosition;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Table;
 use Modules\Cms\Filament\Resources\PageContentResource;
 use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
+use Modules\Xot\Filament\Pages\XotBaseListRecords;
 
 class ListPageContents extends XotBaseListRecords
 {
     use ListRecords\Concerns\Translatable;
-    
+
     protected static string $resource = PageContentResource::class;
 
     public function getGridTableColumns(): array
