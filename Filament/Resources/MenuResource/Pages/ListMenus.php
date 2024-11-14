cb03646dfd438ac3e4c0deb4e635ddf1de2f315e
marco76tv <marco76tv@users.noreply.github.com>
2024-11-14 20:34:38 +0000

    Check & fix styling

--- Filament/Resources/MenuResource/Pages/ListMenus.php
+++ Filament/Resources/MenuResource/Pages/ListMenus.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Modules\Cms\Filament\Resources\MenuResource\Pages;
 
 use Filament\Actions\CreateAction;
-use Filament\Resources\Pages\ListRecords;
 use Modules\Cms\Filament\Resources\MenuResource;
 use Modules\Xot\Filament\Pages\XotBaseListRecords;
 
