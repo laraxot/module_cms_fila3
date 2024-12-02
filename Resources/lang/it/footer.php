600bbdf97a261725cf7ef12c22d698c6b2417568
Davide Tricella <dadi.tricella@gmail.com>
2024-12-02 15:40:59 +0100

    .

---
+++ Resources/lang/it/footer.php
@@ -0,0 +1,16 @@
+<?php return array (
+  'fields' => 
+  array (
+    'view' => 
+    array (
+      'label' => 'view',
+    ),
+  ),
+  'actions' => 
+  array (
+    'updateAction' => 
+    array (
+      'label' => 'updateAction',
+    ),
+  ),
+);
