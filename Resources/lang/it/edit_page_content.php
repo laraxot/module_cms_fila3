4407ca02013501b85e8a29ca07051eff5f49c689
Malebestia <vair81@gmail.com>
2024-12-04 18:01:04 +0100

    feat(lang): add new translations for edit page content, page content builder and view page content in Italian language

---
+++ Resources/lang/it/edit_page_content.php
@@ -0,0 +1,17 @@
+<?php return array (
+  'actions' => 
+  array (
+    'view' => 
+    array (
+      'label' => 'view',
+    ),
+    'delete' => 
+    array (
+      'label' => 'delete',
+    ),
+    'activeLocale' => 
+    array (
+      'label' => 'activeLocale',
+    ),
+  ),
+);
---
+++ Resources/lang/it/page_content_builder.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'fields' => 
+  array (
+    'blocks' => 
+    array (
+      'label' => 'blocks',
+    ),
+  ),
+);
