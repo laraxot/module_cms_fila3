a9a7ace06dbbb2b2cd76b51ac8ed1068507d957d
Davide Tricella <dadi.tricella@gmail.com>
2024-12-05 10:31:50 +0100

    up

---
+++ Resources/lang/it/view_page_content.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'actions' => 
+  array (
+    'edit' => 
+    array (
+      'label' => 'edit',
+    ),
+    'activeLocale' => 
+    array (
+      'label' => 'activeLocale',
+    ),
+  ),
+);
