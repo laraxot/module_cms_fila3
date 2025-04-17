7a047e30bee9097052afb265fb498bad42d028a7
marco76tv <marco.sottana@gmail.com>
2025-04-17 16:17:40 +0200

    first

---
+++ lang/it/menu.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'title' => [
+            'label' => 'Titolo',
+        ],
+        'icon' => [
+            'label' => 'icon',
+        ],
+        'image' => [
+            'label' => 'image',
+        ],
+        'items' => [
+            'label' => 'items',
+        ],
+        'url' => [
+            'label' => 'url',
+        ],
+        'type' => [
+            'label' => 'type',
+        ],
+    ],
+    'navigation' => [
+        'group' => [
+            'name' => 'Site',
+        ],
+        'plural' => 'Menu',
+        'name' => 'Menu',
+    ],
+    'actions' => [
+        'create' => [
+            'tooltip' => 'Crea',
+        ],
+    ],
+];
